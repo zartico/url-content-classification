eb078334053284cca801fda23a5e48aed80f171e
Lauren Morganthaler <laurenmorganthaler@Laurens-MacBook-Pro.local>
2025-08-05 11:14:23 -0600

    Imports cleaned up for categorize

--- src/categorize.py
+++ src/categorize.py
@@ -1,13 +1,11 @@
 from config.project_config import PROJECT_ID, BQ_DATASET_ID, BQ_TABLE_ID
-from utils.cache import hash_url, check_cache_for_urls, update_bq_cache, get_result_columns
-from utils.web_fetch import fetch_all_pages
+from utils.cache import hash_url, get_result_columns
 from utils.category_mapping import map_to_zartico_category
 from utils.utils import is_homepage, check_and_increment_quota
 from google.cloud import language_v1, bigquery
 from bs4 import BeautifulSoup
 from datetime import datetime, timezone
 import pandas as pd
-import asyncio
 import time
 
 
